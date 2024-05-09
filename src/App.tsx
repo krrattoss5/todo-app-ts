bfff59048182a79c70af27af8d0cdac0533715f4
krrattoss5 <krrattoss5@gmail.com>
2024-05-08 21:16:23 -0500

    (add/onRemoveTodo)

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,15 @@ const mockTodos = [
 // podemos tipar un componente como (): JSX.Element o App: React.FC
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ( id: string ): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
   return (
     <div className="todoapp">
-      <Todos todos={todos} />
+      <Todos todos={todos} onRemoveTodo={handleRemove} />
     </div>
   )
 }
