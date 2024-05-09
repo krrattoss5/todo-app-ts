3b9e021ea32b55c4c37ea335e36939d66d568a1e
krrattoss5 <krrattoss5@gmail.com>
2024-05-09 16:29:25 -0500

    (modified/Filters.tsx & consts.tsx): se agregaron diccionarios y un ejemplo de tipado dinamico con typescript y typeof

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,4 +1,12 @@
-import { TODO_FILTERS } from "../consts"
+import { TODO_FILTERS, FILTERS_BUTTONS } from "../consts"
+
+interface Props {
+  // filterSelected: typeof TODO_FILTERS['all'] mala practica no es escalable
+  // filterSelected: typeof TODO_FILTERS['all' | 'active' | 'completed] mala practica no es escalable
+
+  // utiliza una de las keys del tipo de TODO_FILTERS: esto si es escalable
+  filterSelected: typeof TODO_FILTERS[keyof typeof TODO_FILTERS]
+}
 
 export const Filters: React.FC<Props> = (): void => {
   return(
--- src/consts.tsx
+++ src/consts.tsx
@@ -3,3 +3,18 @@ export const TODO_FILTERS = {
   ACTIVE: 'active',
   COMPLETED: 'completed'
 } as const
+
+export const FILTERS_BUTTONS = {
+  [TODO_FILTERS.ALL]:{
+    literal: 'Todos',
+    href: `/?filters=${TODO_FILTERS.ALL}`
+  },
+  [TODO_FILTERS.ACTIVE]:{
+    literal: 'Activos',
+    href: `/?filters=${TODO_FILTERS.ACTIVE}`
+  },
+  [TODO_FILTERS.COMPLETED]:{
+    literal: 'Completados',
+    href: `/?filters=${TODO_FILTERS.COMPLETED}`
+  }
+} as const
